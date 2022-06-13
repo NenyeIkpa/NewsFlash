effcbb80f9c2a4665913e7b5ba2a46d72ef62dd5
NenyeIkpa <nenyeikpa@gmail.com>
2022-06-13 20:55:13 +0100

    setup splash, login and signup screens

--- app/src/main/java/com/example/newsflash/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsflash/presentation/ui/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.newsflash
+package com.example.newsflash.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsflash.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_NewsFlash)
         setContentView(R.layout.activity_main)
+
     }
 }
